551a89ccf5d79c286e1a1df696d0146f391aa4ef
Cornel Barna <cornel.barna@senecacollege.ca>
2024-03-14 16:13:14 -0400

    Workshop #8 has been released!

---
+++ WS08/part 1/utilities.cpp
@@ -0,0 +1,18 @@
+#include <memory>
+#include "list.h"
+#include "element.h"
+#include "utilities.h"
+
+using namespace std;
+
+namespace seneca {
+	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using raw pointers
+
+
+
+		return priceList;
+	}
+}
---
+++ WS08/part 2/utilities.cpp
@@ -0,0 +1,27 @@
+#include <memory>
+#include "list.h"
+#include "element.h"
+#include "utilities.h"
+
+using namespace std;
+
+namespace seneca {
+	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using raw pointers
+
+
+
+		return priceList;
+	}
+
+	List<Product> mergeSmart(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using smart pointers
+
+
+		return priceList;
+	}
+}
